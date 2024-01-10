189bcf55b885138182b77383456f481c678e2358
rada-i <neposebanmail@gmail.com>
2024-01-10 19:54:32 +0100

    corrections

--- src/filters/Search.jsx
+++ src/filters/Search.jsx
@@ -1,7 +1,7 @@
 import { FileSearch } from "phosphor-react";
 
 const Search = ({ handleSearch }) => (
-  <div className="flex justify-stretch text-gray-600">
+  <div className="flex justify-stretch text-gray-700">
     <FileSearch size={32} weight="fill" className=" text-indigo-200" />
     <input
       className=" outline-none border-b"
