39783e2b78f08e4fb643b0274f992e8a704df64b
rada-i <neposebanmail@gmail.com>
2024-01-14 22:39:49 +0100

    corrections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,9 @@
-
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().toLocaleString("en-US", { month: "long" });
 
   return (
-    <footer className="bg-indigo-300 fixed bottom-0 py-3 w-full mt-16">
+    <footer className="bg-indigo-300 fixed bottom-0 py-4 w-full mt-16">
       <div className="container flex mx-12">
         <p className="text-white  text-sm">
           &copy; {currentMonth} {currentYear} By Rada Ivankovic
